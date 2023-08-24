7d68d9538c3e8df4138e376d83c26834c1917039
Phillip Blucas <pblucas@gmail.com>
2023-08-24 16:01:49 -0500

    Fix bugs

--- rootfs/etc/cont-init.d/01-ubxtool.sh
+++ rootfs/etc/cont-init.d/01-ubxtool.sh
@@ -1,16 +1,4 @@
 #!/usr/bin/with-contenv bash
-# shellcheck shell=bash
+#shellcheck shell=bash
 
-APPNAME="01-ubxtool"
-s6wrap=(s6wrap --quiet --prepend="${APPNAME}" --timestamps)
-
-s6wrap --args echo "Setting up /dev/gpsd0 with ubxtool..."
-s6wrap --args ubxtool -P 18 -v 2 -w 2 -s 9600 -S 115200 -f /dev/gpsd0
-s6wrap --args ubxtool -P 18 -v 2 -w 2 -s 115200 -f /dev/gpsd0 -e BINARY
-s6wrap --args ubxtool -P 18 -v 2 -w 2 -s 115200 -f /dev/gpsd0 -d NMEA
-s6wrap --args ubxtool -P 18 -v 2 -w 2 -s 115200 -f /dev/gpsd0 -p MODEL,2
-s6wrap --args ubxtool -P 18 -v 2 -w 2 -s 115200 -f /dev/gpsd0 -d GLONASS
-s6wrap --args ubxtool -P 18 -v 2 -w 2 -s 115200 -f /dev/gpsd0 -d GALILEO
-s6wrap --args ubxtool -P 18 -v 2 -w 2 -s 115200 -f /dev/gpsd0 -d BEIDOU
-s6wrap --args ubxtool -P 18 -v 2 -w 2 -s 115200 -f /dev/gpsd0 -e SBAS
-s6wrap --args ubxtool -P 18 -v 2 -w 2 -s 115200 -f /dev/gpsd0 -p SAVE
+s6-setuidgid root /usr/bin/ublox-setup.sh
---
+++ rootfs/usr/bin/ublox-setup.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/with-contenv bash
+#shellcheck shell=bash
+
+APPNAME="ublox-setup"
+s6wrap=(s6wrap --quiet --timestamps --prepend="${APPNAME}")
+
+s6wrap --args ubxtool -P 18 -v 2 -w 2 -s 9600 -S 115200 -f /dev/gpsd0
+s6wrap --args ubxtool -P 18 -v 2 -w 2 -s 115200 -f /dev/gpsd0 -e BINARY
+s6wrap --args ubxtool -P 18 -v 2 -w 2 -s 115200 -f /dev/gpsd0 -d NMEA
+s6wrap --args ubxtool -P 18 -v 2 -w 2 -s 115200 -f /dev/gpsd0 -p MODEL,2
+s6wrap --args ubxtool -P 18 -v 2 -w 2 -s 115200 -f /dev/gpsd0 -d GLONASS
+s6wrap --args ubxtool -P 18 -v 2 -w 2 -s 115200 -f /dev/gpsd0 -d GALILEO
+s6wrap --args ubxtool -P 18 -v 2 -w 2 -s 115200 -f /dev/gpsd0 -d BEIDOU
+s6wrap --args ubxtool -P 18 -v 2 -w 2 -s 115200 -f /dev/gpsd0 -e SBAS
+s6wrap --args ubxtool -P 18 -v 2 -w 2 -s 115200 -f /dev/gpsd0 -p SAVE
