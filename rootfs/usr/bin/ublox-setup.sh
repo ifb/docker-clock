ff491e8c06d6177e0c2a9259d909b50755759784
Phillip Blucas <pblucas@gmail.com>
2023-09-05 21:47:13 -0500

    Use UBXOPTS

--- rootfs/usr/bin/ublox-setup.sh
+++ rootfs/usr/bin/ublox-setup.sh
@@ -4,7 +4,7 @@
 APPNAME="ublox-setup"
 s6wrap=(s6wrap --quiet --timestamps --prepend="${APPNAME}")
 
-s6wrap --args ubxtool -P 18 -v 0 -s 9600 -S 115200 -f /dev/ttyAMA0
-s6wrap --args ubxtool -P 18 -v 0 -s 115200 -f /dev/ttyAMA0 -p MODEL,2
-s6wrap --args ubxtool -P 18 -v 0 -s 115200 -f /dev/ttyAMA0 -e BINARY -d NMEA -d GLONASS -d GALILEO -d BEIDOU -e SBAS
-s6wrap --args ubxtool -P 18 -v 2 -w 3 -s 115200 -f /dev/ttyAMA0 -p SAVE
+s6wrap --args ubxtool -s 9600 -S 115200
+s6wrap --args ubxtool -s 115200 -p MODEL,2
+s6wrap --args ubxtool -s 115200 -e BINARY -d NMEA -d GLONASS -d GALILEO -d BEIDOU -e SBAS
+s6wrap --args ubxtool -w 3 -s 115200 -p SAVE
